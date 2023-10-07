c82e962a36817bd233a08a45b927dfb8587a60b3
mtali <emmanuelmtali@yahoo.com>
2023-10-07 23:36:08 +0300

    Add custom manager on post model to retrieve published posts

--- blogs/models.py
+++ blogs/models.py
@@ -2,6 +2,11 @@ from django.db import models
 from django.utils import timezone
 
 
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager, self).get_queryset().filter(status='published')
+
+
 class Post(models.Model):
     STATUS_CHOICES = (
         ('draft', 'Draft'),
@@ -15,6 +20,9 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
+
+    objects = models.Manager()
+    published = PublishedManager()
 
     class Meta:
         db_table = 'posts'
