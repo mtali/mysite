42ffdf0ac1fa321fcf5975ebffca3d4ac2d4d8b4
mtali <emmanuelmtali@yahoo.com>
2023-10-07 17:16:51 +0300

    Create blogs app, and add Post model

---
+++ blogs/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.utils import timezone
+
+
+class Post(models.Model):
+    STATUS_CHOICES = (
+        ('draft', 'Draft'),
+        ('published', 'Published'),
+    )
+    title = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250, unique_for_date='publish')
+    author = models.ForeignKey('users.User', on_delete=models.CASCADE, related_name='blog_posts')
+    body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
+
+    class Meta:
+        db_table = 'posts'
+        ordering = ('-publish',)
+
+    def __str__(self):
+        return self.title
