4475defa6f8df3aa5e5db037995135f52f238fc1
mtali <emmanuelmtali@yahoo.com>
2023-10-08 02:32:14 +0300

    Show most commented posts

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,4 +1,5 @@
 from django import template
+from django.db.models import Count
 
 from ..models import Post
 
@@ -14,3 +15,9 @@ def total_posts():
 def show_latest_posts(count=5):
     latest_posts = Post.published.order_by('-publish')[:count]
     return {'latest_posts': latest_posts}
+
+
+@register.inclusion_tag('blog/post/most_commented_posts.html')
+def show_most_commented_posts(count=5):
+    most_commented_posts = Post.published.annotate(total_comments=Count('comments')).order_by('-total_comments')[:count]
+    return {'most_commented_posts': most_commented_posts}
