003f17b16e1c989e81dff6544ee608ba21c54be0
mtali <emmanuelmtali@yahoo.com>
2023-10-08 00:09:28 +0300

    Rename blogs to blog Add blog list and detail page

--- mysite/settings.py
+++ mysite/settings.py
@@ -29,14 +29,16 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'users.apps.UsersConfig',
+    'blog.apps.BlogConfig',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'users.apps.UsersConfig',
-    'blogs.apps.BlogsConfig'
+
 ]
 
 MIDDLEWARE = [
